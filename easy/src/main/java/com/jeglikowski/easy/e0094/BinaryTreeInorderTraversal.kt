e9f55d32b28985aad4dd4e1d4ed47dd252098a28
Kamil Jeglikowski <kamiljeglikowski@users.noreply.github.com>
2025-01-06 11:56:03 +0100

    [EASY-94] Binary tree inorder traversal recursive (#20)

---
+++ easy/src/main/java/com/jeglikowski/easy/e0094/BinaryTreeInorderTraversal.kt
@@ -0,0 +1,34 @@
+package com.jeglikowski.easy.e0094
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+private class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        if (root == null) return emptyList()
+
+        return inorderTraversal(root.left) + root.`val` + inorderTraversal(root.right)
+    }
+}
+
+fun main() {
+    val solution = Solution()
+    val root = TreeNode(1)
+    root.right = TreeNode(2)
+    root.right?.left = TreeNode(3)
+    val result = solution.inorderTraversal(root)
+    println(result) // [1, 3, 2]
+}
